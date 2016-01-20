5135a8f5d2ae0acc295d2211746d7e46a4d080d5
Austin Minnon and Joseph Karasek <joseph.karasek@gmail.com>
2016-01-20 15:25:28 -0800

    middle of doing something

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 // will return false, is not a palindrome
 var isPalindrome = function(userString){
-  return false;
+// will return true, first char = last char
+  for(var i= 0; i= ß)
+  if (userString[0]===userString[userString.length-1]) {
+    return true;
+  } else {
+    return false;
+  }
 }
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,10 @@ describe('isPalindrome',function(){
   it("will return false if not palindrome",function(){
     expect(isPalindrome("mirror")).to.equal(false);
   });
+  it("will return true if it is a palindrome",function(){
+    expect(isPalindrome("abccba")).to.equal(true);
+  });
+  it("will return true if first char. = last char.",function(){
+    expect(isPalindrome("abcabca")).to.equal(true);
+  });
 });
