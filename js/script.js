b13a6595f153e016eefcb411a0dc6ba73a209b3e
Austin Minnon and Joseph Karasek <whi7ee@gmail.com>
2016-01-20 16:17:22 -0800

    finish functionality to demonstrate, enjoy

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
 // will return false, is not a palindrome
+// var isPalindrome = function(userString){
+// // will return true, first char = last char
+//   for(var i = 0; i < userString.length/2; i++){
+//     if (userString[i] !== userString[userString.length-1]) {
+//       return false;
+//     } else {
+//
+//     }
+//   }
+//   if (userString[0] === userString[userString.length-1]) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 var isPalindrome = function(userString){
-// will return true, first char = last char
-  for(var i= 0; i= ß)
-  if (userString[0]===userString[userString.length-1]) {
-    return true;
-  } else {
-    return false;
+  for(var i = 0; i < userString.length/2; i++){
+    if (userString[i] !== userString[userString.length-1-i]) {
+      return false;
+    }
   }
+  return true;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -5,7 +5,7 @@ describe('isPalindrome',function(){
   it("will return true if it is a palindrome",function(){
     expect(isPalindrome("abccba")).to.equal(true);
   });
-  it("will return true if first char. = last char.",function(){
-    expect(isPalindrome("abcabca")).to.equal(true);
+  it("will return false if it is almost a palindrome",function(){
+    expect(isPalindrome("abcasdacba")).to.equal(false);
   });
 });
